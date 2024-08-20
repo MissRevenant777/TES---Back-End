418b979812c17f5367322860ce420bcacea0a53b
Jenny Mäki-Jaakkola <p75756@edu.sasky.fi>
2024-08-20 13:53:48 +0300

    Käsittelijän pohjarunko

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
+  // Siistimisen jälkeen osoite /~jmakijaa/nayttotyo/tapahtuma?id=1 on 
+  // lyhentynyt muotoon /tapahtuma.
+  $request = str_replace('/~jmakijaa/nayttotyo','',$_SERVER['REQUEST_URI']);
+  $request = strtok($request, '?');
+
+  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava 
+  // käsittelijä.
+  if ($request === '/' || $request === '/tapahtumat') {
+    echo '<h1>Upcoming tournaments</h1>';
+  } else if ($request === '/tapahtuma') {
+    echo '<h1>Tournament information</h1>';
+  } else {
+    echo '<h1>The page you requested could not be found! </h1>';
+  }
+
+?> 
