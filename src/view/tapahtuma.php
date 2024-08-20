8467c0dbac30bb9f9bec8bbd08d05cfdc13495f8
Jenny Mäki-Jaakkola <p75756@edu.sasky.fi>
2024-08-20 16:54:56 +0300

    yksittäisen tapahtuman tietojen tulostaminen

--- src/view/tapahtuma.php
+++ src/view/tapahtuma.php
@@ -1,3 +1,11 @@
-<?php $this->layout('template', ['title' => 'Tapahtuma']) ?>
+<?php $this->layout('template', ['title' => $tapahtuma['nimi']]) ?>
 
-<h1>Tournament</h1>
+<?php
+  $start = new DateTime($tapahtuma['tap_alkaa']);
+  $end = new DateTime($tapahtuma['tap_loppuu']);
+?>
+
+<h1><?=$tapahtuma['nimi']?></h1>
+<div><?=$tapahtuma['kuvaus']?></div>
+<div>Tournament starts: <?=$start->format('j.n.Y G:i')?></div>
+<div>Tournament ends: <?=$end->format('j.n.Y G:i')?></div>
---
+++ src/view/tapahtumanotfound.php
@@ -0,0 +1,5 @@
+<?php $this->layout('template', ['title' => '']) ?>
+
+<h1>Whoops!</h1>
+
+<p>Unfortunately, the event you requested could not be found.
