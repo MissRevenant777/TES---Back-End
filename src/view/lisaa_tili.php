a4b578d1409fef7adc3e851158198b1592372c2c
Jenny Mäki-Jaakkola <p75756@edu.sasky.fi>
2024-08-24 14:32:27 +0300

    fixaus

--- src/view/lisaa_tili.php
+++ src/view/lisaa_tili.php
@@ -8,20 +8,20 @@
     <input id="nimi" type="text" name="nimi">
   </div>
   <div>
-    <label for="nimi">Gaming alias:</label>
-    <input id="nimi" type="text" name="nimi">
+    <label for="alias">Gaming alias:</label>
+    <input id="alias" type="text" name="alias">
   </div>
   <div>
-    <label for="nimi">Country:</label>
-    <input id="nimi" type="text" name="nimi">
-  </div>
-  <div>
-    <label for="email">Email:</label>
-    <input id="email" type="email" name="email">
+    <label for="maa">Country:</label>
+    <input id="maa" type="text" name="maa">
   </div>
   <div>
     <label for="discord">Discord-ID:</label>
     <input id="discord" type="text" name="discord">
+  </div>
+  <div>
+    <label for="email">Email:</label>
+    <input id="email" type="email" name="email">
   </div>
   <div>
     <label for="salasana1">Password:</label>
