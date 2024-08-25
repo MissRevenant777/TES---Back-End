e3e75284d1be1a2a882f1539c2614b11b56f364e
Jenny Mäki-Jaakkola <p75756@edu.sasky.fi>
2024-08-25 15:23:31 +0300

    loin kaiken alusta ja päivitin

--- src/controller/tili.php
+++ src/controller/tili.php
@@ -20,36 +20,18 @@ function lisaaTili($formdata) {
   if (!isset($formdata['nimi']) || !$formdata['nimi']) {
     $error['nimi'] = "Enter your name.";
   } else {
-    if (!preg_match("/^[- '\p{L}]+$/u", $formdata["nimi"])) {
+    if (!preg_match("/^[- '\p{L}]+$/u", $formdata['nimi'])) {
       $error['nimi'] = "Enter your name without special characters.";
-    }
-  }
-
-  // Tarkistetaan onko alias määritelty ja se täyttää mallin.
-  if (!isset($formdata['alias']) || !$formdata['alias']) {
-    $error['alias'] = "Enter your alias.";
-  } else {
-    if (!preg_match("/^[- '\p{L}]+$/u", $formdata["alias"])) {
-      $error['alias'] = "Enter your alias without special character.";
-    }
-  }
-
-  // Tarkistetaan onko maa määritelty ja se täyttää mallin.
-  if (!isset($formdata['maa']) || !$formdata['maa']) {
-    $error['maa'] = "Enter your country.";
-  } else {
-    if (!preg_match("/^[- '\p{L}]+$/u", $formdata["maa"])) {
-      $error['maa'] = "Enter your country without special characters.";
     }
   }
 
   // Tarkistetaan, että discord-tunnus on määritelty ja se on
   // muodossa tunnus#0000.
   if (!isset($formdata['discord']) || !$formdata['discord']) {
-    $error['discord'] = "Enter Your Discord-ID.";
+    $error['discord'] = "Enter your Discord in ID#0000 -form.";
   } else {
     if (!preg_match("/^.+#\d{4}$/",$formdata['discord'])) {
-      $error['discord'] = "Wrong Discord-ID form.";
+      $error['discord'] = "Discord-ID is incorrect.";
     }
   }
 
@@ -59,11 +41,7 @@ function lisaaTili($formdata) {
     $error['email'] = "Enter your email.";
   } else {
     if (!filter_var($formdata['email'], FILTER_VALIDATE_EMAIL)) {
-      $error['email'] = "Wrong email form.";
-    } else {
-      if (haeHenkiloSahkopostilla($formdata['email'])) {
-        $error['email'] = "Email is already taken.";
-      }
+      $error['email'] = "Your email is not valid.";
     }
   }
 
@@ -72,10 +50,10 @@ function lisaaTili($formdata) {
   if (isset($formdata['salasana1']) && $formdata['salasana1'] &&
       isset($formdata['salasana2']) && $formdata['salasana2']) {
     if ($formdata['salasana1'] != $formdata['salasana2']) {
-      $error['salasana'] = "Password is not matching!";
+      $error['salasana'] = "Password is not valid!";
     }
   } else {
-    $error['salasana'] = "Please, enter the password.";
+    $error['salasana'] = "Enter twice the same password.";
   }
 
   // Lisätään tiedot tietokantaan, jos edellä syötettyissä
@@ -92,7 +70,7 @@ function lisaaTili($formdata) {
 
     // Lisätään henkilö tietokantaan. Jos lisäys onnistui,
     // tulee palautusarvona lisätyn henkilön id-tunniste.
-    $idhenkilo = lisaaHenkilo($nimi,$alias,$maa,$discord,$email,$salasana);
+    $idhenkilo = lisaaHenkilo($nimi,$email,$discord,$salasana);
 
     // Palautetaan JSON-tyyppinen taulukko, jossa:
     //  status   = Koodi, joka kertoo lisäyksen onnistumisen.
