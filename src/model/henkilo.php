fe10514fbf4056e43d711a9e680823a10f6db5f8
Jenny Mäki-Jaakkola <p75756@edu.sasky.fi>
2024-08-27 14:51:19 +0300

    vaihtoavaimen lisääminen henkilo-tauluun

--- src/model/henkilo.php
+++ src/model/henkilo.php
@@ -13,4 +13,24 @@
     return DB::run('SELECT * FROM henkilo WHERE email = ?;', [$email])->fetch();
   }
 
+  function paivitaVahvavain($email,$avain) {
+    return DB::run('UPDATE henkilo SET vahvavain = ? WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function vahvistaTili($avain) {
+    return DB::run('UPDATE henkilo SET vahvistettu = TRUE WHERE vahvavain = ?', [$avain])->rowCount();
+  }
+
+  function asetaVaihtoavain($email,$avain) {
+    return DB::run('UPDATE henkilo SET nollausavain = ?, nollausaika = NOW() + INTERVAL 30 MINUTE WHERE email = ?', [$avain,$email])->rowCount();
+  }
+
+  function tarkistaVaihtoavain($avain) {
+    return DB::run('SELECT nollausavain, nollausaika-NOW() AS aikaikkuna FROM henkilo WHERE nollausavain = ?', [$avain])->fetch();
+  }
+
+  function vaihdaSalasanaAvaimella($salasana,$avain) {
+    return DB::run('UPDATE henkilo SET salasana = ?, nollausavain = NULL, nollausaika = NULL WHERE nollausavain = ?', [$salasana,$avain])->rowCount();
+  }
+
 ?>
